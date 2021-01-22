fc67c6babf1ef65f16b5f1da14a41388ec7c18e8
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2021-01-22 13:06:54 +0100

    Hashing function

--- src/lib/ipUtils.js
+++ src/lib/ipUtils.js
@@ -3,7 +3,7 @@ const { aton } = require("inet");
 module.exports = {
     getIPv4HashKey: (ip) => {
         const ip_num = aton(ip);
-        const hash = ip_num % 128000000;
+        const hash = ip_num % 128_000_000;
         return hash;
     },
 };
