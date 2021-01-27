6287a861331606294f6aff334b6020437d3ab8b0
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2021-01-27 11:28:39 +0100

    Added version

--- src/dynamodb-generate.js
+++ src/dynamodb-generate.js
@@ -3,17 +3,20 @@ const fs = require("fs");
 const Locations = require("./lib/Locations");
 const Blocks = require("./lib/Blocks");
 
+const IP_TYPE = "IPv4";
+const DATE = new Date().toISOString().split("T")[0].replace(/-/g, "");
+
 (async () => {
     await Locations.loadLocations();
     const readBlocks = readline.createInterface({
         input: fs.createReadStream(
-            __dirname + "/../data/GeoIp2-City-Blocks-IPv4.csv"
+            `${__dirname}/../data/GeoIp2-City-Blocks-${IP_TYPE}.csv`
         ),
         output: false,
         console: false,
     });
     const outFile = await fs.promises.open(
-        __dirname + `/../out/out-with-locations.json`,
+        __dirname + `/../out/${DATE}_out_${IP_TYPE.toLowerCase()}.json`,
         "w"
     );
 
@@ -21,7 +24,7 @@ const Blocks = require("./lib/Blocks");
     let BLOCKS_PER_HASH = 32;
     let current_hash = 0;
     let count = 0;
-    let manifest = [];
+    let manifest_locations = [];
     for await (const line of readBlocks) {
         if (skip) {
             skip = false;
@@ -31,7 +34,7 @@ const Blocks = require("./lib/Blocks");
         const location = Locations.getLocation(entry.gid);
         outFile.write(Blocks.getExportedEntry(current_hash, entry, location));
         if (count === 0) {
-            manifest.push(entry.ips);
+            manifest_locations.push(entry.ips);
         }
         if (count === BLOCKS_PER_HASH - 1) {
             current_hash += 1;
@@ -40,10 +43,17 @@ const Blocks = require("./lib/Blocks");
             count += 1;
         }
     }
+
     // Write manifest
     const manifestFile = await fs.promises.open(
-        __dirname + `/../out/manifest.json`,
+        `${__dirname}/../out/${DATE}-manifest-${IP_TYPE.toLowerCase()}.json`,
         "w"
     );
-    manifestFile.write(JSON.stringify(manifest));
+    manifestFile.write(
+        JSON.stringify({
+            date: DATE,
+            table: `${DATE}_geoip_${IP_TYPE.toLowerCase()}`,
+            locations: manifest_locations,
+        })
+    );
 })();
